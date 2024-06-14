f829c38288f5db6956b9433205c22b4835b80334
Anuj Kanada <128421858+Anuj-Kanada@users.noreply.github.com>
2024-06-14 22:31:14 +0530

    Update about.php

--- about.php
+++ about.php
@@ -4,7 +4,7 @@ session_start();
 
 include("includes/db.php");
 include("includes/header.php");
-include("functions/functions.php");
+include("functions/functions.php");  // to access file we use include
 include("includes/main.php");
 
 ?>
